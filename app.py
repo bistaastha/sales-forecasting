38041ff7059085fd6b55b5d0ace0145b09bd94b5
Aastha Bist <abist119@gmail.com>
2021-05-26 21:01:19 +0530

    Complete: Add support for GET and POST requests

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    example_embed='This string is from python'
+    return render_template('index.html', embed=example_embed)
+
+@app.route('/test', methods=['GET', 'POST'])
+def testfn():    # GET request
+    if request.method == 'GET':
+        message = {'greeting':'Hello from Flask!'}
+        return jsonify(message)  # serialize and use JSON headers    # POST request
+    if request.method == 'POST':
+        print(request.get_json())  # parse as JSON
+        return 'Sucesss', 200
+
+
+app.run(debug=True)
