034f51634b2e33c4f9af148009e03e34e145ef9f
Aastha Bist <abist119@gmail.com>
2021-06-10 23:14:32 +0530

    Added POST req endpoint for feature selection

--- app.py
+++ app.py
@@ -1,21 +1,45 @@
-from flask import Flask, render_template, jsonify, request
+import os
+import pandas as pd
+from flask import Response, Flask, send_file, request
+from flask.templating import render_template
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+import dataframe_image as dfi
+
+from matplotlib.figure import Figure
+matplotlib.use('Agg')
 
 app = Flask(__name__)
-
+out = pd.read_csv('./data/out.csv')
 
 @app.route('/')
-def home_page():
-    example_embed='This string is from python'
-    return render_template('index.html', embed=example_embed)
+def show_template():
+    return render_template('index.html')
 
-@app.route('/test', methods=['GET', 'POST'])
-def testfn():    # GET request
-    if request.method == 'GET':
-        message = {'greeting':'Hello from Flask!'}
-        return jsonify(message)  # serialize and use JSON headers    # POST request
+@app.route('/dataframe')
+def display_frame():
+    out_head = out.head()
+    datapath = os.path.join(os.getcwd() + '/static/img')
+    dfi.export(
+        out_head,
+        os.path.join(datapath, 'out.png')
+    )
+    return render_template('dataframe.html')
+
+@app.route('/heatmap.png')
+def plot_png():
+    datapath = os.path.join(os.getcwd() + '/public', 'out.png')
+    return send_file(datapath, mimetype='image/png')
+
+@app.route('/data', methods = ["POST"])
+def data():
     if request.method == 'POST':
-        print(request.get_json())  # parse as JSON
-        return 'Sucesss', 200
-
-
-app.run(debug=True)
+        print(request.data, flush=True)
+        processing()
+        return 'OK', 200
+    
+if __name__ == "__main__":
+    app.run(port=5000, debug = True)
